600d808e2dbb4573cee8f39aae890f24d15d6929
Ahmadbader17 <56409106+Ahmadbader17@users.noreply.github.com>
2020-10-07 18:53:15 +0530

    Add files via upload

---
+++ 1097B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+using namespace std;
+typedef long long ll;
+int a[20];
+int n;
+int main(){
+
+    
+     #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    cin>>n;
+    for(int i=0;i<n;i++){
+        scanf("%d",a+i);
+    }
+    bool flag=false;
+    for(int i=0;i<1<<n;i++){
+        int ans=0;
+        for(int j=0;j<n;j++){
+            if(i&(1<<j)){
+                ans+=a[j];
+            }else{
+                ans-=a[j];
+            }
+        }
+        if(ans%360==0){
+            flag=true;
+            break;
+        }
+    }
+    if(flag){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    return 0;
+}
